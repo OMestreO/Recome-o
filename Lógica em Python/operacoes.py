bb2ccceaae03d18640487607496ce0b7b607ffab
OMestreO <paulorjob7@gmail.com>
2024-02-23 07:47:22 -0300

    Operações

---
+++ Lógica em Python/operacoes.py
@@ -0,0 +1,9 @@
+A = 3+2 #ADICAO#
+B = 3-2 #SUBTRACAO#
+C = 3*2 #MULTIPLICACAO#
+D = 3/2 #DIVISAO#
+E = 11//3 #QUOCIENTE NUMERO QUE MULTIPLICA O DIVISOR#
+F = 11%3 #RESTO#
+G = 3**2 #POTENCIA#
+
+print(f"Adição: {A} Subtração: {B} Multiplicação: {C} Dvisão: {D} Quociente: {E} Resto: {F} Potencia: {G}")
